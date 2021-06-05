8f37a5a52fb2315f97dca93f72bc444b38558f22
Igor Lima <igor.filipe.lima@gmail.comm>
2021-06-05 22:16:19 +0100

    deploy to heroku

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const { google } = require("googleapis");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/submit", (req, res) => {
+  res.render("index");
+});
+
+app.get("/", async (req, res) => {
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1KX4X7gUW4dq2xsg_I-6DGAWIqI8p8PpNGna27Tc5eoY";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:B",
+  });
+
+  console.log(getRows);
+  var rowValue = getRows.data.values;
+
+  res.render("learn", {
+    word1:'Potato',
+    word2:rowValue,
+
+    }
+    );
+});
+
+
+app.post("/submit", async (req, res) => {
+  const { request, name } = req.body;
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1KX4X7gUW4dq2xsg_I-6DGAWIqI8p8PpNGna27Tc5eoY";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:A",
+  });
+
+  // Write row(s) to spreadsheet
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:B",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: [[request, name]],
+    },
+  });
+
+  // res.send("Successfully submitted! Thank you!");
+});
+
+app.listen(1337, (req, res) => console.log("running on 1337"));
